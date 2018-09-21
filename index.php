5d4cebb0c96a16e19ce02fa6bc34035ac2c08084
Kirill <kir.pastuhov2016@ya.ru>
2018-09-21 13:17:10 +0300

    ...

--- index.php
+++ index.php
@@ -2,20 +2,21 @@
 <html>
 <head>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
     <title>Lab №1</title>
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <style>
         body, html {
             text-align: center;
-            overflow:  hidden;
+            overflow: hidden;
             padding: 0;
             margin: 0;
             background-color: #b4b4b4;
-            height:100%;
+            height: 100%;
             font-family: Monospace, monospace;
             font-size: 19pt;
         }
+
         .inputs {
             text-align: center;
             display: block;
@@ -24,13 +25,16 @@
             padding-bottom: 10px;
             padding-top: 10px;
         }
+
         .graph {
             text-align: center;
             margin-top: 50px;
         }
+
         .sharp {
             border-radius: 0;
         }
+
         .btn-submit {
             color: Black;
             font-size: 24px;
@@ -38,13 +42,16 @@
             background-color: rgba(180, 180, 180, 0.7);
             border-color: Black;
         }
-        .btn-submit:hover{
+
+        .btn-submit:hover {
             background-color: rgba(195, 195, 195, 0.7);
         }
-        .btn-submit:active{
+
+        .btn-submit:active {
             background-color: SlateGrey; /* Цвет кнопки при нажатии */
         }
-        #num{
+
+        #num {
             font-size: 18px;
         }
 
@@ -53,170 +60,174 @@
 </head>
 <body>
 
-    <h3>Пастухов Кирилл Р3118 Вариант 28813</h3>
-    <form method="POST" action="script.php" onsubmit="return Validate()" name="input_form" >
-        <div class="inputs">
-            <div id="x_div">
+<h3>Pastukhov Kirill P3118 Var. 28813</h3>
+<form method="POST" action="script.php" onsubmit="return Validate()" name="input_form">
+    <div class="inputs">
+        <div id="x_div">
 
-                Input X<br>
-                <label id="num">
-                    <input name="inputX" id="inputX" type="radio" value="-4">
-                    <span>-4</span>
-                </label>
+            Input X<br>
+            <label id="num">
+                <input name="inputX" id="inputX" type="radio" value="-4">
+                <span>-4</span>
+            </label>
 
-                <label id="num">
-                    <input name="inputX" id="inputX" type="radio" value="-3">
-                    <span>-3</span>
-                </label>
+            <label id="num">
+                <input name="inputX" id="inputX" type="radio" value="-3">
+                <span>-3</span>
+            </label>
 
-                <label id="num">
-                    <input name="inputX" id="inputX" type="radio" value="-2">
-                    <span>-2</span>
-                </label>
+            <label id="num">
+                <input name="inputX" id="inputX" type="radio" value="-2">
+                <span>-2</span>
+            </label>
 
-                <label id="num">
-                    <input name="inputX" id="inputX" type="radio" value="-1">
-                    <span>-1</span>
-                </label>
+            <label id="num">
+                <input name="inputX" id="inputX" type="radio" value="-1">
+                <span>-1</span>
+            </label>
 
-                <label id="num">
-                    <input name="inputX" id="inputX" type="radio" value="0" checked>
-                    <span>0</span>
-                </label>
+            <label id="num">
+                <input name="inputX" id="inputX" type="radio" value="0" checked>
+                <span>0</span>
+            </label>
 
-                <label id="num">
-                    <input name="inputX" id="inputX" type="radio" value="1">
-                    <span>1</span>
-                </label>
+            <label id="num">
+                <input name="inputX" id="inputX" type="radio" value="1">
+                <span>1</span>
+            </label>
 
-                <label id="num">
-                    <input name="inputX" id="inputX" type="radio" value="2">
-                    <span>2</span>
-                </label>
+            <label id="num">
+                <input name="inputX" id="inputX" type="radio" value="2">
+                <span>2</span>
+            </label>
 
-                <label id="num">
-                    <input name="inputX" id="inputX" type="radio" value="3">
-                    <span>3</span>
-                </label>
+            <label id="num">
+                <input name="inputX" id="inputX" type="radio" value="3">
+                <span>3</span>
+            </label>
 
-                <label id="num">
-                    <input name="inputX" id="inputX" type="radio" value="4">
-                    <span>4</span>
-                </label>
+            <label id="num">
+                <input name="inputX" id="inputX" type="radio" value="4">
+                <span>4</span>
+            </label>
 
-                <div id="x_error"></div>
-            </div>
-            <div id="y_div">
-                Input Y<br>
-                <label>
-                    <input type="text" name="inputY" size="40"  value="">
-                </label>
+            <div id="x_error"></div>
+        </div>
+        <div id="y_div">
+            Input Y<br>
+            <label>
+                <input type="text" name="inputY" size="40" value="" maxlength="8">
+            </label>
 
-                <div id="y_error"></div>
-            </div>
-            <div id="r_div">
-                Input R<br>
-                <label>
-                    <select name="inputR">
-                        <option value='1'>1</option>
-                        <option value='1.5'>1.5</option>
-                        <option value='2'>2</option>
-                        <option value='2.5'>2.5</option>
-                        <option value='3'>3</option>
-                    </select>
-                </label>
+            <div id="y_error"></div>
+        </div>
+        <div id="r_div">
+            Input R<br>
+            <label>
+                <select name="inputR">
+                    <option value='1'>1</option>
+                    <option value='1.5'>1.5</option>
+                    <option value='2'>2</option>
+                    <option value='2.5'>2.5</option>
+                    <option value='3'>3</option>
+                </select>
+            </label>
 
-                <div id="r_error"></div>
-            </div>
+            <div id="r_error"></div>
+        </div>
 
-            <div class="graph">
-                Graph:<br> <img src="graph.jpg">
-            </div>
+        <div class="graph">
+            Graph:<br> <img src="graph.jpg">
+        </div>
 
-            <div class="submit">
-                <button name="btn" type="submit" class="btn-submit sharp" value="False">Submit</button>
-            </div>
-        </form>
-    <script>
+        <div class="submit">
+            <button name="btn" type="submit" class="btn-submit sharp" value="False">Submit</button>
+        </div>
+</form>
+<script>
 
-        let x = document.forms['input_form']['inputX'];
-        let y = document.forms['input_form']['inputY'];
-        let r = document.forms['input_form']['inputR'];
+    let x = document.forms['input_form']['inputX'];
+    let y = document.forms['input_form']['inputY'];
+    let r = document.forms['input_form']['inputR'];
 
-        // SELECTING ALL ERROR DISPLAY ELEMENTS
-        let x_error = document.getElementById('x_error');
-        let y_error = document.getElementById('y_error');
-        let r_error = document.getElementById('r_error');
+    // SELECTING ALL ERROR DISPLAY ELEMENTS
+    let x_error = document.getElementById('x_error');
+    let y_error = document.getElementById('y_error');
+    let r_error = document.getElementById('r_error');
 
-        // SETTING ALL EVENT LISTENERS
-        x.addEventListener('blur', xVerify, true);
-        y.addEventListener('blur', yVerify, true);
-        x.addEventListener('blur', rVerify, true);
-        // validation function
-        /**
-         * @return {boolean}
-         */
-        function Validate() {
+    // SETTING ALL EVENT LISTENERS
+    x.addEventListener(xVerify, true);
+    y.addEventListener(yVerify, true);
+    x.addEventListener(rVerify, true);
 
-            // validate x
-            // if (document.getElementsByName("inputX").checked === false) {
-            //     x.style.border = "1px solid red";
-            //     document.getElementById('x_div').style.color = "red";
-            //     x_error.textContent = "X is required";
-            //     x.focus();
-            //     return false;
-            // }
+    // validation function
+    /**
+     * @return {boolean}
+     */
+    function Validate() {
 
-            if (y.value === "") {
-                y.style.border = "1px solid red";
-                document.getElementById('y_div').style.color = "red";
-                y_error.textContent = "Y is required";
-                y.focus();
-                return false;
-            }
-            if (isNaN(y.value)) {
+        // validate x
+        // if (document.getElementsByName("inputX").checked === false) {
+        //     x.style.border = "1px solid red";
+        //     document.getElementById('x_div').style.color = "red";
+        //     x_error.textContent = "X is required";
+        //     x.focus();
+        //     return false;
+        // }
 
-                y.style.border = "1px solid red";
-                document.getElementById('y_div').style.color = "red";
-                y_error.textContent = "Y must be a number";
-                y.focus();
-                return false;
-            }
-            if (y.value < -5 || y.value > 3) {
-
-                y.style.border = "1px solid red";
-                document.getElementById('y_div').style.color = "red";
-                y_error.textContent = "Y must be in range [-5;3]";
-                y.focus();
-                return false;
-            }
-
+        if (y.value === "") {
+            y.style.border = "1px solid red";
+            document.getElementById('y_div').style.color = "red";
+            y_error.textContent = "Y is required";
+            y.focus();
+            return false;
         }
-        // event handler functions
-        function xVerify() {
-            if (document.getElementById("inputX").checked === true) {
-                x.style.border = "1px solid #5e6e66";
-                document.getElementById('x_div').style.color = "green";
-                x_error.innerHTML = "";
-                return true;
-            }
+        if (isNaN(y.value)) {
+
+            y.style.border = "1px solid red";
+            document.getElementById('y_div').style.color = "red";
+            y_error.textContent = "Y must be a number";
+            y.focus();
+            return false;
         }
-        function yVerify() {
-            if (y.value !== "") {
-                y.style.border = "1px solid #5e6e66";
-                document.getElementById('y_div').style.color = "green";
-                y_error.innerHTML = "";
-                return true;
-            }
+        if (y.value < -5 || y.value > 3) {
+
+            y.style.border = "1px solid red";
+            document.getElementById('y_div').style.color = "red";
+            y_error.textContent = "Y must be in range [-5;3]";
+            y.focus();
+            return false;
         }
-        function rVerify() {
-            if (r.value !== "") {
-                r.style.border = "1px solid #5e6e66";
-                document.getElementById('r_div').style.color = "green";
-                r_error.innerHTML = "";
-                return true;
-            }
+
+    }
+
+    // event handler functions
+    function xVerify() {
+        if (document.getElementById("inputX").checked === true) {
+            x.style.border = "1px solid #5e6e66";
+            document.getElementById('x_div').style.color = "green";
+            x_error.innerHTML = "";
+            return true;
         }
-    </script>
+    }
+
+    function yVerify() {
+        if (y.value !== "") {
+            y.style.border = "1px solid #5e6e66";
+            document.getElementById('y_div').style.color = "green";
+            y_error.innerHTML = "";
+            return true;
+        }
+    }
+
+    function rVerify() {
+        if (r.value !== "") {
+            r.style.border = "1px solid #5e6e66";
+            document.getElementById('r_div').style.color = "green";
+            r_error.innerHTML = "";
+            return true;
+        }
+    }
+</script>
 </body>
 </html>
--- script.php
+++ script.php
@@ -1,131 +1,133 @@
 <html>
-    <head>
-      <meta charset="utf-8" />
-      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-      <title>Results</title>
-        <style>
-            body
-            {
-                background-color: #b4b4b4;
-                height:100%;
-                font-familY: Monospace, monospace;
-                font-size: 19pt;
-            }
-            h3
-            {
-                font-familY: Monospace, monospace;
-                font-size: 19pt;
-            }
+<head>
+    <meta charset="utf-8"/>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>Results</title>
+    <style>
+        body {
+            background-color: #b4b4b4;
+            height: 100%;
+            font-familY: Monospace, monospace;
+            font-size: 19pt;
+        }
 
-            .sharp {
-                border-radius: 0;
-            }
-            .btn-submit {
-    			color: Black;
-    			font-size: 24px;
-    			padding: 15px 71px;
-    			/* background-color: LightSlateGray; */
-    			background-color: rgba(180, 180, 180, 0.7);
-    			border-color: Black;
-            }
-            .btn-submit:hover{
-                /* background-color: SlateGrey;  */
-                background-color: rgba(195, 195, 195, 0.7);
-            }
-            .btn-submit:active{
-                background-color: SlateGrey; /* Цвет кнопки при нажатии */
-            }
+        h3 {
+            font-familY: Monospace, monospace;
+            font-size: 19pt;
+        }
 
-            div[class="table"]{
-                text-align: center;
-          	    display: inline-block;
-                font-familY: Monospace, monospace;
-      			font-size: 25pt;
-                width: 100%;
-                padding-bottom: 10px;
-                padding-top: 10px;
-            }
-            div[class=time]{
-                text-align: center;
-            }
-            div[class=submit]{
-                text-align: center;
-            }
-        </style>
-    </head>
+        .sharp {
+            border-radius: 0;
+        }
+
+        .btn-submit {
+            color: Black;
+            font-size: 24px;
+            padding: 15px 71px;
+            /* background-color: LightSlateGray; */
+            background-color: rgba(180, 180, 180, 0.7);
+            border-color: Black;
+        }
+
+        .btn-submit:hover {
+            /* background-color: SlateGrey;  */
+            background-color: rgba(195, 195, 195, 0.7);
+        }
+
+        .btn-submit:active {
+            background-color: SlateGrey; /* Цвет кнопки при нажатии */
+        }
+
+        div[class="table"] {
+            text-align: center;
+            display: inline-block;
+            font-familY: Monospace, monospace;
+            font-size: 25pt;
+            width: 100%;
+            padding-bottom: 10px;
+            padding-top: 10px;
+        }
+
+        div[class=time] {
+            text-align: center;
+        }
+
+        div[class=submit] {
+            text-align: center;
+        }
+    </style>
+</head>
 
 
-    <body>
-      <div class="time">
+<body>
+<div class="time">
 
-      <?php
-//      echo ini_set('error_reporting', E_ALL);
-//      echo ini_set('display_errors', 1);
-//      echo ini_set('display_startup_errors', 1);
+    <?php
+    //      echo ini_set('error_reporting', E_ALL);
+    //      echo ini_set('display_errors', 1);
+    //      echo ini_set('display_startup_errors', 1);
 
-      $start_time = microtime(true);
-      $X = $_POST['inputX'];
-      $Y = $_POST['inputY'];
-      $R = $_POST['inputR'];
+    $start_time = microtime(true);
+    $X = $_POST['inputX'];
+    $Y = $_POST['inputY'];
+    $R = $_POST['inputR'];
 
-      // coordinates for triangle
-      $A_X = -$R/2;
-      $A_Y = 0;
+    // coordinates for triangle
+    $A_X = -$R / 2;
+    $A_Y = 0;
 
-      $B_X = 0;
-      $B_Y = 0;
+    $B_X = 0;
+    $B_Y = 0;
 
-      $C_X = 0;
-      $C_Y = $R;
+    $C_X = 0;
+    $C_Y = $R;
 
-      function IsInTriangle($A_X, $A_Y, $B_X, $B_Y, $C_X, $C_Y, $X, $Y)
-      {
-          $a = ($A_X - $X) * ($B_Y - $A_Y) - ($B_X - $A_X) * ($A_Y - $Y);
-          $b = ($B_X - $X) * ($C_Y - $B_Y) - ($C_X - $B_X) * ($B_Y - $Y);
-          $c = ($C_X - $X) * ($A_Y - $C_Y) - ($A_X - $C_X) * ($C_Y - $Y);
+    function IsInTriangle($A_X, $A_Y, $B_X, $B_Y, $C_X, $C_Y, $X, $Y)
+    {
+        $a = ($A_X - $X) * ($B_Y - $A_Y) - ($B_X - $A_X) * ($A_Y - $Y);
+        $b = ($B_X - $X) * ($C_Y - $B_Y) - ($C_X - $B_X) * ($B_Y - $Y);
+        $c = ($C_X - $X) * ($A_Y - $C_Y) - ($A_X - $C_X) * ($C_Y - $Y);
 
-          if (($a >= 0 && $b >= 0 && $c >= 0) || ($a <= 0 && $b <= 0 && $c <= 0))
-          {
+        if (($a >= 0 && $b >= 0 && $c >= 0) || ($a <= 0 && $b <= 0 && $c <= 0)) {
             return true;
-          }
-          else
-          {
+        } else {
             return false;
-          }
+        }
 
-        }
-        function IsInCircle($R, $X, $Y)
-        {
-          if ($X >= 0 && $X <= ($R/2) &&
-              $Y >= 0 && $Y <= ($R/2) &&
-             ($X * $X + $Y * $Y) <= ($R * $R))
-             {
-               return true;
-             }
-          else {
+    }
+
+    function IsInCircle($R, $X, $Y)
+    {
+        if ($X >= 0 && $X <= ($R / 2) &&
+            $Y >= 0 && $Y <= ($R / 2) &&
+            ($X * $X + $Y * $Y) <= ($R * $R)) {
+            return true;
+        } else {
             return false;
-          }
         }
-        $IN = false;
-        if (!is_numeric($Y))
-        {
-            $IN ='Неверный формат Y!';
+    }
+
+    $IN = false;
+    if (!is_numeric($Y)) {
+        $IN = 'Неверный формат Y!';
+    } elseif (($Y > 3) || ($Y < -5)) {
+        $IN = 'Y не попадает в заданный диапазон [-5, 3]!';
+    } else {
+        if (($X >= 0 && $X <= $R / 2 && $Y <= 0 && $Y >= -$R)) {
+            $IN = "IN";
+        } elseif (IsInCircle($R, $X, $Y)) {
+            $IN = "IN";
+        } elseif (IsInTriangle($A_X, $A_Y, $B_X, $B_Y, $C_X, $C_Y, $X, $Y)) {
+            $IN = "IN";
+        } else {
+            $IN = "OUT";
         }
-        elseif(($Y>3)||($Y<-5))
-        {
-            $IN = 'Y не попадает в заданный диапазон [-5, 3]!';
-        }
-        else {
-            if(($X >= 0 && $X <= $R/2 && $Y <= 0 && $Y >= -$R)) {$IN = "IN";}
-            elseif (IsInCircle($R, $X, $Y)) {$IN = "IN";}
-            elseif (IsInTriangle($A_X, $A_Y, $B_X, $B_Y, $C_X, $C_Y, $X, $Y)) {$IN = "IN";}
-            else {$IN = "OUT";}
-            $exec_time = (microtime(true) - $start_time) * pow(10,6);
-            echo ("<br>Execution time: ".$exec_time."<br>");
-            echo ("Current time: ".date("H:i:s <br><br>"));
-          } ?>
-      </div>
-      <div class="table">
+        $exec_time = (microtime(true) - $start_time) * pow(10, 6);
+        echo("<br>Execution time: " . $exec_time . "<br>");
+        echo("Current time: " . date("H:i:s <br><br>"));
+    } ?>
+</div>
+<div class="table">
       <span style="font-size: x-small; font-family: Courier New,monospace; ">
       <table border="1" width="100%" cellpadding="5" align-center>
        <tr>
@@ -141,9 +143,9 @@
         <td id="result" align="center"><?php echo $IN ?></td>
       </tr>
      </table>
-   </div>
-    <div class="submit">
-          <button onclick="history.go(-1);" class="btn-submit sharp">Back</button>
-    </div>
-    </body>
+</div>
+<div class="submit">
+    <button onclick="history.go(-1);" class="btn-submit sharp">Back</button>
+</div>
+</body>
 </html>
